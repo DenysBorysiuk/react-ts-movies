f24ed31f27fe019c537e04fa5b6b8645cdaa0268
Denys Borysiuk <denisborisiuk@gmail.com>
2023-09-12 17:29:54 +0300

    udate movie details

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,16 +1,16 @@
-import { useState, useEffect, useRef, Suspense } from "react";
-import { useParams, Outlet, useLocation } from "react-router-dom";
-import { getMovieDetails } from "../services/api";
-import MovieInfo from "../components/MovieInfo/MovieInfo";
-import BackLink from "../components/BackLink/BackLink";
-import toast from "react-hot-toast";
-import Loader from "../components/Loader/Loader";
+import { useState, useEffect, useRef, Suspense } from 'react';
+import { useParams, Outlet, useLocation } from 'react-router-dom';
+import { getMovieDetails } from '../services/api';
+import MovieInfo from '../components/MovieInfo/MovieInfo';
+import BackLink from '../components/BackLink/BackLink';
+import toast from 'react-hot-toast';
+import Loader from '../components/Loader/Loader';
 
-const MovieDetails: React.FC = () => {
+const MovieDetails = () => {
   const { movieId } = useParams() as { movieId: string };
   const [movie, setMovie] = useState(null);
   const location = useLocation();
-  const ref = useRef(location.state?.from ?? "/");
+  const ref = useRef(location.state?.from ?? '/');
 
   useEffect(() => {
     const controller = new AbortController();
@@ -20,8 +20,8 @@ const MovieDetails: React.FC = () => {
         const movieDetails = await getMovieDetails(movieId, signal);
         setMovie(movieDetails);
       } catch (error: any) {
-        if (error.name === "CanceledError") return;
-        toast.error("Oops, something went wrong");
+        if (error.name === 'CanceledError') return;
+        toast.error('Oops, something went wrong');
       }
     };
 
